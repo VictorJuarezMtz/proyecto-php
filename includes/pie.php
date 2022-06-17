0ba07f566d4134cfd04f457a7890fe6ee4b3687e
VictorJuarezMtz <64214942+VictorJuarezMtz@users.noreply.github.com>
2022-06-17 18:08:11 -0500

    cambios

---
+++ includes/pie.php
@@ -0,0 +1,10 @@
+        <div class="clearfix"></div>
+        </div>
+ <!-- pie-->
+    <footer id="pie">
+        <p>desarrollado por victor juarez martinez &copy; 2022 </p>
+
+    </footer>
+       
+</body>
+</html>
