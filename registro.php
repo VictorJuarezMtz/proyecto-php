f9b37cab309fc7673a2e37207fdb22f2ed6a4167
VictorJuarezMtz <64214942+VictorJuarezMtz@users.noreply.github.com>
2022-06-20 17:51:30 -0500

    proceso insert

--- registro.php
+++ registro.php
@@ -1,9 +1,19 @@
 <?php
+if (isset($_POST['submit'])) {
+    //recolectar los valores del formulario
+    echo $nombre =       isset($_POST['nombre']) ? $_POST['nombre']: false;
+    echo $apellidos = isset($_POST['apellidos']) ? $_POST['apellidos']: false;
+    echo $email = isset($_POST['email']) ? $_POST['email'] : false;
+    echo $password = isset($_POST['pw']) ? $_POST['pw'] : false;
 
 
-var_dump($_POST);
+    //validar los datods a la bd 
+    if (!empty($nombre) && !is_numeric($nombre) && !preg_match("/0-9/",$nombre )) {
+        echo "es nombre es valido";
+    }
 
 
+}
 
 
 ?>
